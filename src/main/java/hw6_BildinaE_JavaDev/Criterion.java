7ffcae9ae6849cebf89ff4558b75e6467a01e88e
EkaterinaBildina <kukresch@gmail.com>
2023-05-22 00:01:25 +0300

    progress Java in API home work. 6lesson to do

---
+++ src/main/java/hw6_BildinaE_JavaDev/Criterion.java
@@ -0,0 +1,60 @@
+package hw6_BildinaE_JavaDev;
+import java.util.*;
+class Criterion {
+    Object value;
+    Double minValue;
+    Double maxValue;
+    boolean isQuantitative;
+    String property;
+
+    public Criterion(String property, boolean isQuantitative, Object value, Double minValue, Double maxValue) {
+        this.property = property;
+        this.isQuantitative = isQuantitative;
+        this.value = value;
+        this.minValue = minValue;
+        this.maxValue = maxValue;
+    }
+
+    public static Criterion startGetting(Scanner scanner, String property, boolean isQuantitative) {
+
+        if (isQuantitative) {
+
+            String quest = "Выберите тип криетрия: " +
+                    "\n 1. Значение" +
+                    "\n 2. Меньше" +
+                    "\n 3. Больше" +
+                    "\n 4. Интервал";
+            System.out.println(quest);
+
+            String text = scanner.next();
+            Criterion criterion = null;
+
+            if (text.equals("1")) {
+                System.out.println("Введите значение поиска: ");
+                int getValue = scanner.nextInt();
+                criterion = new Criterion(property, true, getValue, null, null);
+            } else if (text.equals("2")) {
+                System.out.println("Введите максимальное предельное значение: ");
+                double getValue = scanner.nextDouble();
+                criterion = new Criterion(property, true, null, null, getValue);
+            } else if (text.equals("3")) {
+                System.out.println("Введите минимальное предельное значение: ");
+                double getValue = scanner.nextDouble();
+                criterion = new Criterion(property, true, null, getValue, null);
+            } else if (text.equals("4")) {
+                System.out.println("Введите минимальное предельное значение: ");
+                double getMin = scanner.nextDouble();
+                System.out.println("Введите максимальное предельное значение: ");
+                double getMax = scanner.nextDouble();
+                criterion = new Criterion(property, true, null, getMin, getMax);
+            }
+
+            return criterion;
+        }
+
+        System.out.println("Введите значение поиска: ");
+        String getValue = scanner.next();
+        return new Criterion(property, false, getValue, null, null);
+    }
+
+}
